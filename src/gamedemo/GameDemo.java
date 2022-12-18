2f74a64cfbba5391f3597c461f9e542bc8727b39
emt <110316124@gms.tcu.edu.tw>
2022-12-18 14:40:04 +0800

    改一下

--- src/gamedemo/GameDemo.java
+++ src/gamedemo/GameDemo.java
@@ -1,6 +1,13 @@
 package gamedemo;
 
-import javax.swing.JFrame;
+//import gamedemo.scenes.MainScenes;
+import gamedemo.utils.controllers.ImageController;
+//import gamedemo.utils.core.GIImpl;
+import gamedemo.utils.core.GameKernel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 /**
  *
@@ -9,11 +16,32 @@ import javax.swing.JFrame;
 public class GameDemo {
     public static void main(String[] args) {
         final JFrame jf = new JFrame();
-        jf.setTitle("Demo Game");
+        jf.setTitle("小朋友下樓梯");
         jf.setSize(800, 600);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 按X會關閉
-        
-        
+        int command[][] = {
+                {KeyEvent.VK_W, KeyEvent.VK_W},
+                {KeyEvent.VK_A, KeyEvent.VK_A},
+                {KeyEvent.VK_S, KeyEvent.VK_S},
+                {KeyEvent.VK_D, KeyEvent.VK_D},
+                {KeyEvent.VK_SPACE, KeyEvent.VK_SPACE},
+        };
+        GameKernel.GameInterface gi = new GameKernel.GameInterface() {
+            int x = 30;
+            @Override
+            public void paint(Graphics g) {
+                g.drawImage(ImageController.instance().tryGetImage("r/player_1.png"),0,0,null);
+            }
+
+            @Override
+            public void update() {
+                x += 1;
+            }
+        };
+        GameKernel gk=new GameKernel.Builder(gi,60,60)
+                .gen();
+        jf.add(gk);
         jf.setVisible(true);
+        gk.run(false);
     }
 }
