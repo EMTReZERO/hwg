c073fdcc5ce9849d5583740888e0d80590e474bd
emt <110316124@gms.tcu.edu.tw>
2022-12-18 23:09:13 +0800

    改成物件

--- src/gamedemo/scenes/s2.java
+++ src/gamedemo/scenes/s2.java
@@ -1,6 +1,7 @@
 package gamedemo.scenes;
 
 //import gamedemo.utils.controllers.AudioResourceController;
+import gamedemo.objects.Player;
 import gamedemo.utils.controllers.ImageController;
 import gamedemo.utils.core.CommandSolver;
 import gamedemo.utils.core.Scene;
@@ -10,6 +11,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class s2 extends Scene {
+    Player player;
     int x,y,b;
 //    Animator ar,al,as;
 
@@ -17,13 +19,8 @@ public class s2 extends Scene {
     public void sceneBegin() {
         x=30;
         y=30;
-//        b=0;
-//        ar=new Animator("../../../r/roll_right.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
-//        al=new Animator("../../../r/roll_left.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
-//        as=new Animator("../../../r/select.png",5,22,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
-
-//        new int[](0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19),20
-        //AudioResourceController.getInstance().shot("../../../r/bg.wav");
+        b=0;
+        player=new Player(x,y);
     }
 
     @Override
@@ -33,36 +30,18 @@ public class s2 extends Scene {
 
     @Override
     public void paint(Graphics g) {
-
+        player.paint(g);
         g.drawImage(ImageController.instance().tryGetImage("../../../r/ceiling.png"),0,0,800,16,null );
         g.drawImage(ImageController.instance().tryGetImage("../../../r/wall.png"),770,0,18,600,null );
         g.drawImage(ImageController.instance().tryGetImage("../../../r/wall.png"),0,0,18,600,null );
-//        if(b==0){
-////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_1.png"),x,y,null );
-//            as.paint(x,y,g);
-//        }
-//        else if(b==1)
-//        {
-////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_4.png"),x,y,null );
-////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_5.png"),x,y,null );
-//            al.paint(x,y,g);
-//
-//        }
-//        else
-//        {
-////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_2.png"),x,y,null );
-////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_3.png"),x,y,null );
-//            ar.paint(x,y,g);
-//        }
-
     }
 
     @Override
     public void update() {
-
+        //player.update();
         if(b==2)
         {
-//            ar.update();
+            player.update();
             x+=10;
             if(x>739)
             {
@@ -71,17 +50,17 @@ public class s2 extends Scene {
         }
         else if(b==1)
         {
-//            al.update();
+            player.update();
             x-=10;
             if(x<18)
             {
                 x=18;
             }
         }
-//        else
-//        {
-//            as.update();
-//        }
+        else
+        {
+            player.update();
+        }
     }
 
     @Override
@@ -96,16 +75,20 @@ public class s2 extends Scene {
             public void keyPressed(int commandCode, long trigTime) {
                 if(commandCode== KeyEvent.VK_LEFT)
                 {
+                    player.setState(player.LEFT);
                     b=1;
+
                 }
                 else if(commandCode== KeyEvent.VK_RIGHT)
                 {
+                    player.setState(player.RIGHT);
                     b=2;
                 }
             }
 
             @Override
             public void keyReleased(int commandCode, long trigTime) {
+                player.setState(player.STAND_STILL);
                 b=0;
             }
 
