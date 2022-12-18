3d3e73d47de0622c09961ab4f9cf4b3a96e62749
emt <110316124@gms.tcu.edu.tw>
2022-12-18 15:47:37 +0800

    製作場景

--- src/gamedemo/GameDemo.java
+++ src/gamedemo/GameDemo.java
@@ -1,13 +1,13 @@
 package gamedemo;
 
-//import gamedemo.scenes.MainScenes;
-import gamedemo.utils.controllers.ImageController;
-//import gamedemo.utils.core.GIImpl;
+import gamedemo.utils.core.GIImpl;
 import gamedemo.utils.core.GameKernel;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
 import java.awt.event.KeyEvent;
+
+import static gamedemo.utils.Global.FRAME_LIMIT;
+import static gamedemo.utils.Global.UPDATE_TIMES_PER_SEC;
 
 /**
  *
@@ -16,29 +16,65 @@ import java.awt.event.KeyEvent;
 public class GameDemo {
     public static void main(String[] args) {
         final JFrame jf = new JFrame();
-        jf.setTitle("小朋友下樓梯");
+        jf.setTitle("Demo Game");
         jf.setSize(800, 600);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 按X會關閉
-        int command[][] = {
-                {KeyEvent.VK_W, KeyEvent.VK_W},
-                {KeyEvent.VK_A, KeyEvent.VK_A},
-                {KeyEvent.VK_S, KeyEvent.VK_S},
-                {KeyEvent.VK_D, KeyEvent.VK_D},
-                {KeyEvent.VK_SPACE, KeyEvent.VK_SPACE},
+        int[][] command={
+                {KeyEvent.VK_LEFT,KeyEvent.VK_LEFT},
+                {KeyEvent.VK_RIGHT,KeyEvent.VK_RIGHT},
+                /*{KeyEvent.VK_UP,KeyEvent.VK_UP},
+                {KeyEvent.VK_DOWN,KeyEvent.VK_DOWN},
+                {KeyEvent.VK_A,KeyEvent.VK_LEFT},
+                {KeyEvent.VK_D,KeyEvent.VK_RIGHT},
+                {KeyEvent.VK_W,KeyEvent.VK_UP},
+                {KeyEvent.VK_S,KeyEvent.VK_DOWN},*/
+                {KeyEvent.VK_SPACE,KeyEvent.VK_SPACE},
         };
-        GameKernel.GameInterface gi = new GameKernel.GameInterface() {
-            int x = 30;
+        /*GameKernel.GameInterface gi = new GameKernel.GameInterface() {
+            int x = 30, y = 30;
+
             @Override
             public void paint(Graphics g) {
-                g.drawImage(ImageController.instance().tryGetImage("r/player_1.png"),0,0,null);
+                g.setColor(Color.red);
+                g.drawOval(x,y, 100, 100);
             }
 
             @Override
             public void update() {
-                x += 1;
+                x ++;
             }
-        };
-        GameKernel gk=new GameKernel.Builder(gi,60,60)
+        };*/
+        GIImpl gi=new GIImpl(new DemoScene());
+        GameKernel gk = new GameKernel.Builder(gi,FRAME_LIMIT,UPDATE_TIMES_PER_SEC)
+                .initListener(command)
+                .enableMouseTrack(gi)
+                .enableKeyboardTrack(gi)
+                /*.enableMouseTrack((e, state, trigTime) -> {
+                    if(state!=null){
+                        System.out.println(state);
+                    }
+                })*/
+                .mouseForceRelease()
+                /*.enableKeyboardTrack(new CommandSolver.KeyListener() {
+                    @Override
+                    public void keyPressed(int commandCode, long trigTime) {//按下
+                        System.out.println(commandCode+"按下");
+                    }
+
+                    @Override
+                    public void keyReleased(int commandCode, long trigTime) {//放開
+                        System.out.println(commandCode+"放開");
+                    }
+
+                    @Override
+                    public void keyTyped(char c, long trigTime) {
+                        System.out.println(c+"按了什麼");
+                    }
+                })*/
+                .keyCleanMode() // detect release one times.
+                //.keyTypedMode() // detect one times.
+                // up two function will use the second one, first will be covered.
+                .trackChar() // when write this KeyTyped() will enable else not work.
                 .gen();
         jf.add(gk);
         jf.setVisible(true);
---
+++ src/gamedemo/scenes/s1.java
@@ -0,0 +1,38 @@
+package gamedemo.scenes;
+
+import gamedemo.utils.core.CommandSolver;
+import gamedemo.utils.core.Scene;
+
+import java.awt.*;
+
+public class s1 extends Scene {
+    @Override
+    public void sceneBegin() {
+        
+    }
+
+    @Override
+    public void sceneEnd() {
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public CommandSolver.MouseCommandListener mouseListener() {
+        return null;
+    }
+
+    @Override
+    public CommandSolver.KeyListener keyListener() {
+        return null;
+    }
+}
