b12b1d470b1bc4dd61795a284051bbbbb49575d1
emt <110316124@gms.tcu.edu.tw>
2022-12-19 00:05:49 +0800

    新增磚塊class

---
+++ src/gamedemo/objects/Obstacle.java
@@ -0,0 +1,21 @@
+package gamedemo.objects;
+
+import gamedemo.utils.gameobjects.GameObject;
+
+import java.awt.*;
+
+public class Obstacle extends GameObject {
+    public Obstacle(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+    }
+}
