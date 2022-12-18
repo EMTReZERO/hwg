857a9e1b49143f9b33fb39dfdddbc3f406df343c
emt <110316124@gms.tcu.edu.tw>
2022-12-18 22:56:32 +0800

    改成物件

--- src/gamedemo/scenes/s2.java
+++ src/gamedemo/scenes/s2.java
@@ -11,15 +11,16 @@ import java.awt.event.KeyEvent;
 
 public class s2 extends Scene {
     int x,y,b;
-    Animator ar,al,as;
+//    Animator ar,al,as;
+
     @Override
     public void sceneBegin() {
         x=30;
         y=30;
-        b=0;
-        ar=new Animator("../../../r/roll_right.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
-        al=new Animator("../../../r/roll_left.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
-        as=new Animator("../../../r/select.png",5,22,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
+//        b=0;
+//        ar=new Animator("../../../r/roll_right.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
+//        al=new Animator("../../../r/roll_left.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
+//        as=new Animator("../../../r/select.png",5,22,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
 
 //        new int[](0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19),20
         //AudioResourceController.getInstance().shot("../../../r/bg.wav");
@@ -36,23 +37,23 @@ public class s2 extends Scene {
         g.drawImage(ImageController.instance().tryGetImage("../../../r/ceiling.png"),0,0,800,16,null );
         g.drawImage(ImageController.instance().tryGetImage("../../../r/wall.png"),770,0,18,600,null );
         g.drawImage(ImageController.instance().tryGetImage("../../../r/wall.png"),0,0,18,600,null );
-        if(b==0){
-//            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_1.png"),x,y,null );
-            as.paint(x,y,g);
-        }
-        else if(b==1)
-        {
-//            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_4.png"),x,y,null );
-//            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_5.png"),x,y,null );
-            al.paint(x,y,g);
-
-        }
-        else
-        {
-//            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_2.png"),x,y,null );
-//            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_3.png"),x,y,null );
-            ar.paint(x,y,g);
-        }
+//        if(b==0){
+////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_1.png"),x,y,null );
+//            as.paint(x,y,g);
+//        }
+//        else if(b==1)
+//        {
+////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_4.png"),x,y,null );
+////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_5.png"),x,y,null );
+//            al.paint(x,y,g);
+//
+//        }
+//        else
+//        {
+////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_2.png"),x,y,null );
+////            g.drawImage(ImageController.instance().tryGetImage("../../../r/player_3.png"),x,y,null );
+//            ar.paint(x,y,g);
+//        }
 
     }
 
@@ -61,7 +62,7 @@ public class s2 extends Scene {
 
         if(b==2)
         {
-            ar.update();
+//            ar.update();
             x+=10;
             if(x>739)
             {
@@ -70,17 +71,17 @@ public class s2 extends Scene {
         }
         else if(b==1)
         {
-            al.update();
+//            al.update();
             x-=10;
             if(x<18)
             {
                 x=18;
             }
         }
-        else
-        {
-            as.update();
-        }
+//        else
+//        {
+//            as.update();
+//        }
     }
 
     @Override
---
+++ src/player.java
@@ -0,0 +1,30 @@
+import gamedemo.utils.gameobjects.Animator;
+import gamedemo.utils.gameobjects.GameObject;
+
+import java.awt.*;
+
+public class player extends GameObject {
+    public static final int STAND_STILL=0;
+    public static final int RIGHT=1;
+    public static final int LEFT=2;
+    private int c;
+    Animator[]a;
+    public player(double x, double y) {
+        super(x, y, 30, 32);
+        a=new Animator[3];
+        a[STAND_STILL]=new Animator("../../../r/select.png",5,22,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
+        a[RIGHT]=new Animator("../../../r/roll_right.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
+        a[LEFT]=new Animator("../../../r/roll_left.png",5,30,32,new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19});
+        c=STAND_STILL;
+    }
+
+    @Override
+    public void update() {
+        a[c].update();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        a[c].paint((int) painter().left(),(int) painter().top(),g);
+    }
+}
